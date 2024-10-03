48e13891cd4020b978f5f3ebfbaed690889a96aa
Sampath Surineni <sampath.surineni@gmail.com>
2024-10-04 01:17:58 +0530

    show hamburger when screen is less than 1150

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,11 +1,23 @@
 import { NavLink } from "react-router-dom";
 import './Navbar.css';
+import { IoMenu } from 'react-icons/io5';
 
 const Navbar = () => {
     return (
         <header className="header">
-            <nav className="nav">
+            <nav className="nav container">
                 <NavLink to="/" className="nav__logo">Navigation Bar</NavLink>
+                <div className="nav__menu" id="nav-menu">
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <NavLink to="/" className="nav__link">Home</NavLink>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="nav__toggle" id="nav-toggle">
+                    <IoMenu />
+                </div>
             </nav>
         </header>
     )
