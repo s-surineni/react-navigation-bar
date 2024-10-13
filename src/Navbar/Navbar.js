2c766c06bc177251ee6bc91184ab78627bd7d637
Sampath Surineni <sampath.surineni@gmail.com>
2024-10-13 22:51:10 +0530

    Showing hiding menu on hamburger click

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,21 +1,30 @@
 import { NavLink } from "react-router-dom";
 import './Navbar.css';
-import { IoMenu } from 'react-icons/io5';
+import { useState } from 'react';
+import { IoMenu, IoClose } from 'react-icons/io5';
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
     return (
         <header className="header">
             <nav className="nav container">
                 <NavLink to="/" className="nav__logo">Navigation Bar</NavLink>
-                <div className="nav__menu" id="nav-menu">
+                <div className={`nav__menu ${showMenu ? 'show-menu' : ''}`} id="nav-menu">
                     <ul className="nav__list">
                         <li className="nav__item">
                             <NavLink to="/" className="nav__link">Home</NavLink>
                         </li>
                     </ul>
+                    <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+                        <IoClose />
+                    </div>
                 </div>
 
-                <div className="nav__toggle" id="nav-toggle">
+                <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
                     <IoMenu />
                 </div>
             </nav>
